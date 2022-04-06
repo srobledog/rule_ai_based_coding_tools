e22f00a1b3ccaee848d1d65837b5d20fa2e738cb
srobledog <sebastian@coreofscience.org>
2022-04-06 10:44:30 -0500

    style improved

--- scientometric_analysis.py
+++ scientometric_analysis.py
@@ -3,7 +3,5 @@ from matplotlib import pyplot as plt
 
 df = pd.read_csv("data/scopus_dt_count.csv")
 
-plt.plot(df.DT, df.FREQUENCY, label = "Ransom")
+plt.plot(df.DT, df.FREQUENCY, label="Ransom")
 plt.show()
-
-
