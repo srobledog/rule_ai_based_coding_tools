7c7e0b01c8424ce49555f9f63ca3dd4b7631db28
srobledog <sebastian@coreofscience.org>
2022-04-06 10:40:50 -0500

    added a small code in python

--- scientometric_analysis.py
+++ scientometric_analysis.py
@@ -1,29 +1,9 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 
+df = pd.read_csv("data/scopus_dt_count.csv")
 
-scopus_py = r.scopus
-scopus_py.hist(column = "TC")
-
-scopus_py["TC_log"] = np.log10(scopus_py["TC"] + 1)
-
-scopus_py.hist(column = "TC_log")
-
-plt.figure()
-
-
-import seaborn as sns
-from palmerpenguins import load_penguins
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-
-n = 256
-X = np.linspace(-np.pi,np.pi,n,endpoint=True)
-Y = np.sin(2*X)
-
-plt.plot (X, Y+1, color='blue', alpha=1.00)
-plt.plot (X, Y-1, color='blue', alpha=1.00)
+plt.plot(df.DT, df.FREQUENCY, label = "Ransom")
 plt.show()
+
+
